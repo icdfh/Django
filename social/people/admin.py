a91caa7fa28e1996464e705f75e437165a5b8fa9
icdfh <madiyar.jetibergen@narxoz.kz>
2023-03-06 14:44:44 +0600

    update

--- social/people/admin.py
+++ social/people/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+
+ admin.site.registrer(People, PeopleAdmin)
