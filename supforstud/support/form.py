d162dbe523ad98c5fb1ca1430d2367f6a3fed4b0
icdfh <madiyar.jetibergen@narxoz.kz>
2023-04-26 19:17:42 +0600

    last change

--- supforstud/support/form.py
+++ supforstud/support/form.py
@@ -28,21 +28,21 @@ class AddPostForm(forms.ModelForm):
 
 
 class RegisterUserForm(UserCreationForm):
-   username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
-   email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
-   password1 = forms.CharField(label='Passwords', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-   password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+   username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-group', 'placeholder':'Login'}))
+   email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-group' , 'placeholder':'Email'}))
+   password1 = forms.CharField(label='Passwords', widget=forms.PasswordInput(attrs={'class': 'form-group' , 'placeholder':'Enter ur password'}))
+   password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-group',  'placeholder':'Re password'}))
 
    class Meta:
        model = User
        fields = ('username', 'email', 'password1', 'password2')
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-group','placeholder':'Enter username'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-group ','placeholder':'Enter password'}))
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Name', max_length=255)
     email = forms.EmailField(label='Email')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols':60, 'rows': 10}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols':25, 'rows': 10}))
     captcha = CaptchaField()
